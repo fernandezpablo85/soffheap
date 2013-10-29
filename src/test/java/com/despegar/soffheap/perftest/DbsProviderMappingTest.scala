6aae2e9a515465724239fa2b32d66308321672ac
Diego Parra <diegolparra@gmail.com>
2013-10-29 12:27:22 -0300

    Readme

--- src/test/java/com/despegar/soffheap/perftest/DbsProviderMappingTest.scala
+++ src/test/java/com/despegar/soffheap/perftest/DbsProviderMappingTest.scala
@@ -2,12 +2,21 @@ package com.despegar.soffheap.perftest
 
 import com.despegar.soffheap.ProviderMappingInfo
 import java.util.Random
+import com.despegar.soffheap.metrics.SoffHeapMetricsRegistryHolder
 
 object DbsProviderMappingTest extends App {
 
   val elements = System.getProperty("elements").toInt
   val readers = System.getProperty("readers").toInt
   val writers = System.getProperty("writers").toInt
+
+  val registry =  SoffHeapMetricsRegistryHolder.getMetricsRegistry()
+  val reporter = GangliaReporter.forRegistry(registry)
+                                .convertRatesTo(TimeUnit.SECONDS)
+                                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                                .build(ganglia)
+
+  reporter.start(1, TimeUnit.MINUTES)
 
   val arrayWriter: (Unit => java.util.ArrayList[ProviderMappingInfo]) = { Unit =>
     val list =  new java.util.ArrayList[ProviderMappingInfo]()
